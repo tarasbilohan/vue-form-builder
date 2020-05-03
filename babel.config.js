febbec8c976638fbdc251ec3ad8c084c1eae1052
Taras Bilohan <taras.bilohan@gmail.com>
2020-05-03 13:59:23 +0300

    Add @babel/runtime-corejs3 and correct babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: [
-    '@babel/plugin-transform-runtime'
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 3
+      }
+    ]
   ],
   presets: [
     '@vue/cli-plugin-babel/preset'
